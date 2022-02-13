91673589b033997ba39285735308c7c05931f879
leigharriane <81524834+leigharriane@users.noreply.github.com>
2022-02-14 00:31:03 +0800

    packages

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("body-parser")
+require("dotenv").config();
+
+const app = express();
+const db = mysql.createPool({
+  hostname: "transactionmanagement1.mysql.database.azure.com",
+  user: "adminuser",
+  password: "Password!23",
+  database: "stadvdbmco2",
+  port: 3306,
+});
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.get("/readAll", (req, res) => {
+  const sqlRead = "SELECT * FROM mco2.movies LIMIT 10";
+  db.query(sqlRead, (err, result) => {
+    if (err) console.log("ERROR: "+err);
+    res.send(result);
+  });
+});
+app.post("/createNew",(req,res)=>{
+    const movieName = req.body.name;
+    const movieYear = req.body.year;
+    const movieRank = req.body.rank;
+    const sqlInsert = "INSERT INTO mco2.movies (name, year, rank) VALUES (?,?)"
+    db.query(sqlInsert,[movieName,movieYear,movieRank],(err, result)=>{
+        if (err) console.log("Error: "+err);
+        console.log("Success")
+    })
+})
+app.listen(3001, () => {
+  console.log("Running on port 3001");
+});
